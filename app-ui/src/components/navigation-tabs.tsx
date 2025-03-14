adc0b97baf6054b09117a9f7f0ff3521f2ad4c00
Pratik Ghare <pratikghare888@gmail.com>
2025-03-14 19:39:36 +0530

    Cart Basic, Navigation Radius

--- app-ui/src/components/navigation-tabs.tsx
+++ app-ui/src/components/navigation-tabs.tsx
@@ -19,7 +19,7 @@ export default function NavigationTabs() {
 
     return (
         <div className="fixed bottom-0 pb-2 flex justify-center w-full ">
-            <Tabs size="lg" aria-label="Options" color="primary"
+            <Tabs size="lg" aria-label="Options" color="primary" radius="sm"
                 variant={theme === "light" ? "bordered" : "solid"} selectedKey={selected.key}
                 className="backdrop-blur-sm rounded-2xl"
                 onSelectionChange={(key) => changeTab(key)}
@@ -27,7 +27,7 @@ export default function NavigationTabs() {
                 {
                     navigationTabs.map((tab: NavigationTab) => (
                         <Tab
-                            className={tab.className}
+                            className={tab.className + " rounded-sm"}
                             key={tab.key}
                             shouldSelectOnPressUp
                             onClick={() => changeTab(tab)}
--- app-ui/src/pages/cart.tsx
+++ app-ui/src/pages/cart.tsx
@@ -1,9 +1,24 @@
+import { Button } from "@heroui/react";
+
 export function Cart() {
     return (
-        <section className="grid mt-5 mx-0">
+        <section className="grid mt-5 mx-0 cursor-default">
             <div className="mb-4 flex justify-between items-center mx-[5%] sm:mx-[5%] lg:mx-[20%]">
-                <div className="md:flex-2 z-50">
-                        HI FROM CART
+                <div className="md:flex-1 z-50">
+                        {
+                            true ? 
+                            <>
+                                <h2 className="text-xl flex items-end mb-2">
+                                    <span className="text-medium">Subtotal</span>
+                                    <span className="ml-4 flex">
+                                        <span className="text-xs">₹</span>
+                                        <b>{ "2093" }</b>
+                                    </span>
+                                </h2>
+                                <Button radius="sm" className="w-full">Proceed to buy</Button>
+                            </> 
+                            : <></>
+                        }
                 </div>
             </div>
         </section>
